99ff025a955a6906f73a6c29e1fdeb44130c9b39
wangliangliang <wangliangliang@xiaobing.ai>
2024-10-28 15:27:31 +0800

    controller change

--- src/main/java/com/tencent/wxcloudrun/controller/HelloController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/HelloController.java
@@ -1,13 +1,12 @@
 package com.tencent.wxcloudrun.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * index控制器
  */
-@Controller
-
+@RestController
 public class HelloController {
 
   /**
